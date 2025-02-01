8c6e4e301f808b4bff291cdcb528e91c07c95b8a
Bernapai <juanemeunier@gmail.com>
2025-02-01 04:01:38 -0300

    Changes 3.5 Debugin RabbitMQ Error

--- Microservices/ventasMicro/config/rabbitmq.py
+++ Microservices/ventasMicro/config/rabbitmq.py
@@ -1,12 +1,19 @@
 import pika
 
 def get_rabbitmq_connection():
-    # Establece la conexión con RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
+    try:
+        # Establece la conexión con RabbitMQ
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
 
-    # Declara la cola 'ventas_a_productos'
-    channel.queue_declare(queue='update_stock', durable=True)
+        # Declara la cola 'update_stock' (con durable=True para asegurar la persistencia)
+        channel.queue_declare(queue='update_stock', durable=True)
 
-    print("Cola 'update_stoc' creada exitosamente.")
-    return connection, channel
+        print("Cola 'update_stock' creada exitosamente.")
+
+        # Devuelve la conexión y el canal para que puedan ser usados en otras funciones
+        return connection, channel
+
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Error al conectar con RabbitMQ: {e}")
+        return None, None
